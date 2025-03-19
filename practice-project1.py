1191376cd1899724cc109fa69668003eeca13237
Guan Chunli <guancl2006@163.com>
2025-03-19 11:21:47 +0800

    practice-python1

---
+++ practice-project1.py
@@ -0,0 +1,21 @@
+# a="20"
+# print(a)
+# gcy="successful"
+# print(gcy)
+# first_name = "wang"
+# last_name = "anyu"
+# full_name = first_name + " " + last_name
+# print("Hello " + full_name.title() + ", would you like to learn some Python today?")
+# name = "wang anyu"
+# print(name.title())
+# name = "Wang Anyu"
+# print(name.upper())
+# name = "Wang Anyu"
+# print(name.lower())
+# name = "AE"
+# saying = '"A person who never made a mistake never tried anything new."'
+# print(name + " once said," + saying)
+print(5+3)
+print(2*4)
+print(16/2)
+print(10-2)
